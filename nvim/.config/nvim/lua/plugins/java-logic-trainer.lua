06a3ee3b0addc910bc38f13029923ea71da085fd
NachoOsella <nachoosella03@gmail.com>
2026-05-06 12:09:39 -0300

    update rofi and assets

---
+++ nvim/.config/nvim/lua/plugins/java-logic-trainer.lua
@@ -0,0 +1,11 @@
+return {
+  "NachoOsella/java-logic-trainer.nvim",
+  lazy = false,
+  keys = {
+    { "<leader>js", "<cmd>JavaLogicStart<cr>", desc = "Java Logic: start" },
+    { "<leader>jc", "<cmd>JavaLogicCheck<cr>", desc = "Java Logic: check" },
+    { "<leader>jn", "<cmd>JavaLogicNext<cr>", desc = "Java Logic: next" },
+    { "<leader>jh", "<cmd>JavaLogicHint<cr>", desc = "Java Logic: hint" },
+    { "<leader>jr", "<cmd>JavaLogicReset<cr>", desc = "Java Logic: reset progress" },
+  },
+}
