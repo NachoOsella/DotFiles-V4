c12a128994ffdf1f7171c9c8042c5f8998b53b58
NachoOsella <nachoosella03@gmail.com>
2026-09-12 21:26:07 -0300

    remove bad extensions

--- pi/.pi/agent/extensions/pi-zen-free/headers.ts
+++
@@ -1,4 +0,0 @@
-/** Create one OpenCode request identifier for a logical provider request. */
-export function createOpenCodeRequestId(): string {
-    return `req_${crypto.randomUUID().replaceAll('-', '')}`
-}
